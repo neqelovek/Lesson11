8c387b3fa4083cb8dcfc9bcee619bef7b45e32cd
neqelovek <neqelovek@gmail.com>
2022-01-23 17:00:20 +0300

    Lesson11 finish

--- app/src/main/java/ru/gb/lesson11/ui/slideshow/MainExpensesFragment.java
+++ app/src/main/java/ru/gb/lesson11/ui/slideshow/MainExpensesFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,22 +17,22 @@ import ru.gb.lesson11.databinding.FragmentMainExpensesBinding;
 public class MainExpensesFragment extends Fragment {
 
     private MainExpensesViewModel mainExpensesViewModel;
-private FragmentMainExpensesBinding binding;
+    private FragmentMainExpensesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         mainExpensesViewModel =
                 new ViewModelProvider(this).get(MainExpensesViewModel.class);
 
-    binding = FragmentMainExpensesBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentMainExpensesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textSlideshow;
         mainExpensesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
